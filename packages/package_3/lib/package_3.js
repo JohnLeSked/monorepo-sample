7f52221e6e8649cd15072c4833a1325f61a19ee4
JohnLeSked <nguyen.le@skedulo.com>
2021-06-25 14:57:14 +0700

    commit cirlce ci 2nd time

--- packages/package_3/lib/package_3.js
+++ packages/package_3/lib/package_3.js
@@ -6,7 +6,7 @@ module.exports = package2;
 
 function package3() {
     console.log(package_1.package_1())
-    console.log("This is package_3, verion 1.0.3")
+    console.log("This is package_3, verion 1.0.3, test for circleci")
 }
 
 package3()
