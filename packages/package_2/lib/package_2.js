2a68874b046cc1f2c913faab07d770b63ab967fc
JohnLeSked <nguyen.le@skedulo.com>
2021-06-25 14:16:57 +0700

    package 2 and 3 bump version

--- packages/package_2/lib/package_2.js
+++ packages/package_2/lib/package_2.js
@@ -6,7 +6,7 @@ module.exports = package2;
 
 function package2() {
     console.log(package_1.package_1())
-    console.log("This is package_2")
+    console.log("This is package_2, version 1.0.2")
 }
 
 package2()
