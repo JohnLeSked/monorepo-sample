c75b03fecedce0477a175bfecf5a2fec82b65151
JohnLeSked <nguyen.le@skedulo.com>
2021-06-25 14:59:06 +0700

    commit cirlce ci for package 1

--- packages/package_1/index.js
+++ packages/package_1/index.js
@@ -1,3 +1,3 @@
 module.exports.package_1 = () => {
-    return "This is common package ( package_1 ), change something else"
+    return "This is common package ( package_1 ), change something else, for circleci"
 }
